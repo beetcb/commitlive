97f123bc35dbbdd2d785961d1af514fbe7eed988
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-24 23:25:48 +0800

    fix: remove EOL at the start of prompt

--- src/prompts.js
+++ src/prompts.js
@@ -1,9 +1,9 @@
 const c = require('chalk')
 const prompts = {
-  [c`\n{blue description› }`]: '<type>[optional scope]: <description>',
-  [c`\n{blue body› }`]: '[optional body]',
-  [c`\n{blue footer› }`]: '[optional footer(s)]',
-  [c`\n{green Your commit message is: <hide>}`]: '',
+  [c`{blue description› }`]: '<type>[optional scope]: <description>',
+  [c`{blue body› }`]: '[optional body]',
+  [c`{blue footer› }`]: '[optional footer(s)]',
+  [c`{green Your commit message is: <hide>}`]: '',
 }
 
 module.exports = prompts
